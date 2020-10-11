e1f0c07b70219140b224d64986c06053b23e9902
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2020-10-11 22:50:58 +0200

    Update

--- read_file.py
+++ read_file.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 # read dataset
 
-training = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
-sample_sub = pd.read_csv("sample_submission.csv")
-vocab = open("vocabulary.txt", 'r').read().split("\n")
+train_set = pd.read_csv("data/train.csv")
+test_set = pd.read_csv("data/test.csv")
+sample_sub = pd.read_csv("data/sample_submission.csv")
+vocab = open("data/vocabulary.txt", 'r').read().split("\n")
 
-print(f"size of training {training.shape}")
-print(f"size of testing {test.shape}")
+print(f"size of training {train_set.shape}")
+print(f"size of testing {test_set.shape}")
 # print(type(vocab))
 
 # Prepare training set
@@ -24,21 +24,21 @@ def onehot_sentence(sentence):
         l[vocab.index(label)] = 1
     return l
 
-test_vocab = np.zeros((training.shape[0], len(vocab)), dtype=np.float32)
+test_vocab = np.zeros((train_set.shape[0], len(vocab)), dtype=np.float32)
 
-for i in range(training.shape[0]):
-    test_vocab[i] = onehot_sentence(training.SENTENCE.iloc[i])
+for i in range(train_set.shape[0]):
+    test_vocab[i] = onehot_sentence(train_set.SENTENCE.iloc[i])
 
 # shuffle index
-np.random.shuffle(training)
-np.random.shuffle(test)
+# np.random.shuffle(train_set)
+# np.random.shuffle(test_set)
 
 # split training set into actual training set and validation set
 from sklearn.model_selection import train_test_split as splitter
-x_train, x_val, y_train, y_val = splitter(training, test_vocab, test_size=0.2, random_state=0)
+x_train, x_val, y_train, y_val = splitter(train_set, test_vocab, test_size=0.2, random_state=0)
 
 # Build model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.losses import BinaryCrossentropy
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.losses import BinaryCrossentropy
 
-Model = Sequential()
+# Model = Sequential()
