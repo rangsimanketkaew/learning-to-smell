55a4a19491d321a6af4449acc60cdfdf9aa9f99b
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2020-10-22 14:50:30 +0200

    Fixed model.fit

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from rdkit.Chem import AllChem, rdMolDescriptors, Descriptors
 from rdkit.Chem.EState import Fingerprinter
 # Sklearn
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_validate, train_test_split
 # TensorFlow and Keras for deep learning
 import tensorflow as tf
 from keras.models import Sequential
@@ -22,92 +22,39 @@ from keras.callbacks import ReduceLROnPlateau
 from keras.optimizers import SGD
 
 
-def preprocess_graph(data):
-    # The function is to preprocessed the adjacency matrix,
-    # returning the normalized adjacency matrix in the form of numpy array for feeding into the model
-    adj_ = data + sp.eye(data.shape[0])
-    rowsum = np.array(adj_.sum(1))
-    degree_mat_inv_sqrt = np.diag(np.power(rowsum, -0.5).flatten())
-    adj_normalized = adj_.dot(
-        degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt)
-    return np.array(adj_normalized)
-
-
-def smiles_get_features(a):
-    # This function will return the smiles code into list of feature for each atoms
-    m = rdkit.Chem.MolFromSmiles(a)
-    featurizer = dc.feat.ConvMolFeaturizer()
-    features = featurizer.featurize([m])[0]
-    if isinstance(features, np.ndarray):
-        return pd.np.nan
-    atom_features = features.get_atom_features()  # initial atom feature vectors
-    if atom_features.shape[0] > 60:
-        return pd.np.nan
-    return atom_features
-
-
-def smiles_get_adj(a):  # This function retrieve the adjacency matrix from the molecule
-    m = rdkit.Chem.MolFromSmiles(a)
-    featurizer = dc.feat.ConvMolFeaturizer()
-    features = featurizer.featurize([m])[0]
-    if isinstance(features, np.ndarray):
-        return pd.np.nan
-    adj_list = features.get_adjacency_list()  # adjacency list (neighbor list)
-    # convert adjacency list into adjacency matrix "A"
-    adj = np.zeros((len(adj_list), len(adj_list)))
-    if len(adj_list) > 60:
-        return pd.np.nan
-    return adj_list
-
-
-def sim_graph(smile):
-    mol = rdkit.Chem.MolFromSmiles(smile)
-    if mol is None:
-        return pd.np.nan
-    Chem.Kekulize(mol)
-    atoms = [atom.GetAtomicNum() for atom in mol.GetAtoms()]
-    am = Chem.GetAdjacencyMatrix(mol, useBO=True)
-    if len(atoms) > 60:
-        return pd.np.nan
-    for i, atom in enumerate(atoms):
-        am[i, i] = atom
-    return am
-
-
-def get_max_dim(d):  # This funcion is used to find the maximum dimension the set of data contain
-    maxdim = 0
-    for i in d:
-        if i.shape[0] > maxdim:
-            maxdim = i.shape[0]
-    return maxdim
-
-
-# This function is used to pad the data up to a given dimension
-def pad_up_to(t, max_in_dims, constant_values=0):
-    s = t.shape
-    size = np.subtract(max_in_dims, s)
-    return np.pad(t, ((0, size[0]), (0, size[1])), 'constant', constant_values=constant_values)
-
-
 ###############
 # Read dataset
 ###############
-train_set = pd.read_csv("data/train.csv")
+whole_train_set = pd.read_csv("data/train.csv")
 test_set = pd.read_csv("data/test.csv")
-sample_sub = pd.read_csv("data/sample_submission.csv")
 vocab = open("data/vocabulary.txt", 'r').read().split("\n")
+sample_sub = pd.read_csv("data/sample_submission.csv")
 
-# print(f"size of training {train_set.shape}")
-# print(f"size of testing {test_set.shape}")
+###############
+# Split dataset
+###############
 
-train_set, train_label = list(train_set['SMILES']), list(train_set['SENTENCE'])
+train_set, train_label = list(whole_train_set['SMILES']), list(whole_train_set['SENTENCE'])
+
+train_set, valid_set, train_label, valid_label = train_test_split(
+    train_set, train_label, test_size=0.2, random_state=42)
+
 test_set = list(test_set['SMILES'])
 
-train_label = [i.split(',') for i in train_label]
+print("Train set      : ", len(train_set))
+print("Train label    : ", len(train_label))
+print("Validate set   : ", len(valid_set))
+print("Validate label : ", len(valid_label))
+print("Real Train set : ", len(test_set))
 
-####################
-# Encode and decode
-####################
+# print(train_label)
+train_label = [i.split(',') for i in train_label]
+valid_label = [i.split(',') for i in valid_label]
+# print(train_label)
+
+##############
+# Encode input
+##############
 
 SMILES_CHARS = [
     '#', '%', '(', ')', '+', '-', '.', '/',
@@ -130,8 +77,11 @@ def smiles_encoder(smiles, maxlen=240):
         X[i, smi2index[c]] = 1
     return X
 
-# train_set_enc = np.array([smiles_encoder(i) for i in train_set])
+
+train_set_enc = np.array([smiles_encoder(i) for i in train_set])
+valid_set_enc = np.array([smiles_encoder(i) for i in valid_set])
 # np.savez_compressed("data/train_set_enc", mol=train_set_enc)
+# np.savez_compressed("data/valid_set_enc", mol=valid_set_enc)
 
 
 def smiles_decoder(X):
@@ -143,46 +93,52 @@ def smiles_decoder(X):
 
 # dec = smiles_decoder(mat[0])
 
-
-# Encoder Label (smell)
-lab = LabelEncoder()
-lab.fit(vocab)
-# print(lab.classes_)
-# lab.inverse_transform()
-
-train_label_enc = [lab.transform(i) for i in train_label]
-# print(train_label_enc)
-
-###############
-# Encode vocab
-###############
-
+################
+# Encoder Label
+################
 
 def onehot_sentence(sentence):
     l = np.zeros(len(vocab))
-    for label in sentence.split(','):
+    for label in sentence:
         l[vocab.index(label)] = 1
     return l
 
+# trainint set
+train_label_enc = np.zeros((len(train_label), len(vocab)), dtype=np.float32)
 
-test_vocab = np.zeros((train_set.shape[0], len(vocab)), dtype=np.float32)
-for i in range(train_set.shape[0]):
-    test_vocab[i] = onehot_sentence(train_set.SENTENCE.iloc[i])
-# print(test_vocab[1])
-print(test_vocab.shape)
+for i in range(len(train_label)):
+    train_label_enc[i] = onehot_sentence(train_label[i])
+print(train_label_enc.shape)
 
+# valid set
+valid_label_enc = np.zeros((len(valid_label), len(vocab)), dtype=np.float32)
+
+for i in range(len(valid_label)):
+    valid_label_enc[i] = onehot_sentence(valid_label[i])
+print(valid_label_enc.shape)
+
+# lab = LabelEncoder()
+# lab.fit(vocab)
+# print(lab.classes_)
+# lab.inverse_transform()
+
+# train_label_enc = [lab.transform(i) for i in train_label]
+# valid_label_enc = [lab.transform(i) for i in valid_label]
+# print(train_label_enc)
 
 #################
 # Build the model
 #################
 
-data = np.load("data/train_set_enc.npz")
-mol = data['mol']
-print(mol.shape)
+# data = np.load("data/train_set_enc.npz")
+# mol = data['mol']
+# print(mol.shape)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(240, 55)),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(109)
 ])
 
@@ -190,12 +146,20 @@ model.compile(optimizer='adam',
               loss="binary_crossentropy",
               metrics=['accuracy'])
 
-model.fit(mol, test_vocab, epochs=10)
+print(len(train_set_enc))
+print(len(train_label_enc))
+print(train_label_enc)
+model.fit(train_set_enc, train_label_enc, epochs=200)
 
 ####################
 # Evaluate accuracy
 ####################
 
-test_loss, test_acc = model.evaluate(test_set,  test_label, verbose=2)
+test_loss, test_acc = model.evaluate(valid_set_enc,  valid_label_enc, verbose=1)
 
 print('\nTest accuracy:', test_acc)
+
+
+##########
+# Predict
+##########
