58556d3ff74296b6112676cba010e39fa08eaca9
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2020-10-28 00:14:49 +0100

    Fix Jaccard functions

--- metric.py
+++ metric.py
@@ -1,33 +1,30 @@
 import tensorflow as tf
-from tensorflow.python.framework.ops import disable_eager_execution
-disable_eager_execution()
+# from tensorflow.python.framework.ops import disable_eager_execution
+# disable_eager_execution()
 from tensorflow.keras import backend as K
 
-def jaccard_5sentences(y_true, y_pred):
-    """Jaccard score for judging the performance of model
-    Compare all 15 labels with the ground-truths.
-    """
-    set1 = set(y_true.numpy())
-    set2 = set(y_pred.numpy())
-    jac = float((len(set1.intersection(set2))) / (len(set1.union(set2))))
-    return jac
 
-def jaccard_5sentences_tensor(y_true, y_pred):
-    """Jaccard score for judging the performance of model
-    Compare all 15 labels with the ground-truths.
+def jaccard_tensorflow(y_true, y_pred):
+    """Jaccard score of Tensor in tensorflow for graph mode.
     """
-    print(type(y_true))
-    print(type(y_pred))
     intersection = tf.sets.intersection(y_true[None:], y_pred[None:])
     intersection = tf.sparse.to_dense(intersection)[0]
     union = tf.sets.union(y_true[None:], y_pred[None:])
     union = tf.sparse.to_dense(union)[0]
-    jac = float(len(intersection) / len(union))
-    return jac
+    return float(len(intersection) / len(union))
 
-def jaccard_each_sentences(y_true, y_pred):
-    """Jaccard score for judging the performance of model
-    Compare each sentence (3 labels) with the ground-truths and choose the highest score.
+
+def jaccard_tensorflow_eager(y_true, y_pred):
+    """Jaccard score with built-in function in tensorflow in eager mode.
+    """
+    set1 = set(y_true.numpy())
+    set2 = set(y_pred.numpy())
+    return float((len(set1.intersection(set2))) / (len(set1.union(set2))))
+
+
+def jaccard_from_keras_cont(y_true, y_pred):
+    """Jaccard score for keras.
+    Taken directly from https://github.com/keras-team/keras-contrib/blob/master/keras_contrib/losses/jaccard.py
     """
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
